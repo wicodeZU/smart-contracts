7e846dc79a290a9b83cd0e5c40bc175a7de65c22
Carver <kerenketepela@gmail.com>
2022-03-02 20:16:15 +0300

    feat:add contarct

--- contracts/hello.sol
+++ contracts/hello.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: Unlicense
+// version 0.8.0
 pragma solidity ^0.8.0;
 
 contract HelloWorld{
-    //
+    //hello function
+    function hello() public pure returns(string memory){
+        return "hello";
+    }
 }
